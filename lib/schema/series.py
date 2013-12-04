ff5dbc98400ff969a009eb98c503d88cb1201f21
enki-labs <enki-labs@nomail.no>
2013-12-05 00:08:35 +0100

    Add series data type

--- lib/schema/series.py
+++ lib/schema/series.py
@@ -3,19 +3,27 @@ Define the series table.
 
 """
 
-from sqlalchemy import Column, types
+from sqlalchemy import Column, Sequence, types
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates
+from sqlalchemy.dialects.postgresql import ARRAY
+
 
 def table (base):
     class table_def (base):
         __tablename__ = 'series'
         
-        symbol = Column(types.Unicode(255), primary_key=True)
-        meta = Column(types.UnicodeText())
+        id = Column(types.Integer, Sequence('series_id_seq'), primary_key=True)
+        tags = Column(ARRAY(types.Integer))
+
+        @validates('tags')
+        def _set_tags (self, key, value):
+            """ Ensure tag array always sorted """
+            return sorted(value)
 
         def __repr__ (self):
-            return "<series(symbol='%s', meta='%s')>" % (
-                   self.symbol, self.meta)
+            return "<series(id='%s', tags='%s')>" % (
+                   self.id, self.tags)
 
     return table_def
 
---
+++ test/lib_schema_series.py
@@ -0,0 +1,20 @@
+
+from nose.tools import eq_, ok_
+import config.test
+from lib import common
+from lib import schema
+
+
+def test_insert ():
+    
+    unsorted = [456, 123, 980]
+    sortd = [123, 456, 980]
+
+    insert = schema.table("series")
+    insert.tags = unsorted
+    schema.save(insert)
+
+    inserted = schema.select_one("series", tags=sortd)
+    ok_(inserted != None)
+    eq_(inserted.tags, sortd)
+
