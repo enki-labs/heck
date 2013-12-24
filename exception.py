98586f19ac05b30a00aa81bf65620937299eef84
enki-labs <enki-labs@nomail.no>
2013-12-24 03:52:35 +0100

    Adding common exception defs

---
+++ exception.py
@@ -0,0 +1,11 @@
+
+class MissingTagException (Exception):
+    pass
+
+class OverlapException (Exception):
+    """ Data insertion would overlap existing data """
+    pass
+
+class TimeoutException (Exception):
+    pass
+
